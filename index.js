6c03ae22cb01392efd3f09d91227fcfdb75dd9b6
Ken Barlow <kenbrlow@gmail.com>
2021-03-01 15:20:23 -0700

    commit tree b4b8e90ef3f2ce13f0a8c5e11b5e201efc813f27

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+
+document.addEventListener("keydown", function(e) {
+    console.log(e.key);
+
+    if (e.key == "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key == "ArrowRight") {
+        moveDodgerRight();
+    }
+    
+});
+
+function moveDodgerLeft() {
+    let numbers = dodger.style.left.replace("px", "");
+    let left = parseInt(numbers, 10);
+    if (left > 0) {
+        console.log('dodger.style.left = ' + dodger.style.left);
+        dodger.style.left = `${left - 10}px`;
+    }
+} 
+
+
+function moveDodgerRight() {
+    let numbers = dodger.style.left.replace("px", "");
+    let left = parseInt(numbers, 10);
+    if (left > 0) {
+        console.log('dodger.style.left = ' + dodger.style.left);
+        dodger.style.left = `${left + 10}px`;
+    }
+} 
